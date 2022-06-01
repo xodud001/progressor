efa01a2ec2a3176752ca9f3bb259e406f7623dce
김태영 <freeptk1@gmail.com>
2022-06-01 20:57:10 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/weather/progressor/ProgressorApplication.java
@@ -0,0 +1,13 @@
+package com.weather.progressor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgressorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weather/progressor/ProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.progressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
