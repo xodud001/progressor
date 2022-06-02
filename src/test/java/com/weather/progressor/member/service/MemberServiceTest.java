24ac91a0e628c0d3b4cc50426b76da4a1fbda5c9
김태영 <freeptk1@gmail.com>
2022-06-02 22:26:06 +0900

    test: MemberRepository, MemberService 테스트
    issue: #1

--- src/test/java/com/weather/progressor/member/service/MemberServiceTest.java
+++ src/test/java/com/weather/progressor/member/service/MemberServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
 
 import static java.util.Optional.*;
 import static org.assertj.core.api.Assertions.*;
